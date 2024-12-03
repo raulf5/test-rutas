0b082b7c9effa4c4d80d61aa811e54a22a72a9fb
Raul Garcia <raul.garcia@factoriaf5.org>
2024-12-03 18:09:48 +0100

    add first-version

---
+++ src/main.js
@@ -0,0 +1,28 @@
+
+
+import { setupCounter } from "./assets/js/counter";
+
+import viteLogo from "/vite.svg";
+import javascriptLogo from "./assets/img/javascript.svg";
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+        <a href="/about">About</a> |
+    <a href="/blog/article">Article</a>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+export default {
+    root: "./src",
+    build: {
+        outDir: "../docs",
+        emptyOutDir: true,
+        
+    }
+  };
